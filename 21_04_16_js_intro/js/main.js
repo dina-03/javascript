6cb2ba2c4d2609e626832ba6e86e0384c961279d
dina-03 <dingrafikweb@gmail.com>
2021-04-16 22:51:43 +0200

    javascript 01

---
+++ 21_04_16_js_intro/js/main.js
@@ -0,0 +1,21 @@
+console.log("Hello world")
+
+function fi() {
+    console.log("inside fi")
+}
+
+const a = 10;
+const b = f2(a);
+console.log('b equals to ${b}')
+
+function f2(number) {
+    return number * 2;
+}
+
+let numValue = Number(false)
+let numValue2 = Number(true)
+
+let value = 0 || '23'
+
+
+
