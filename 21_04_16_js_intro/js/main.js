65dcaa7d90d405178b24be61df78657602ab2961
dina-03 <dingrafikweb@gmail.com>
2021-04-22 18:49:23 +0200

    JS DOM

--- 21_04_16_js_intro/js/main.js
+++ 21_04_16_js_intro/js/main.js
@@ -1,4 +1,4 @@
-console.log("Hello world")
+/*console.log("Hello world")
 
 function fi() {
     console.log("inside fi")
@@ -17,5 +17,30 @@ let numValue2 = Number(true)
 
 let value = 0 || '23'
 
+//это комментарий для javascript
+/!*alert(3 + 2 + 1);
+alert("Hi, liebe Freunde");*!/
+let message = 'Hello!';
+alert(message);
+let age=25;
+let name='Johnson';
+alert(name);
+//alert(age);
+age=30;
+alert(age)
+const COLOR_RED='#F00';
+let color = COLOR_RED;
+alert(color);*/
 
+vasya = {
+    name: "Vasya",
+    "age": 23,
+}
 
+function passObject(obj) {
+    for (let field in obj) {
+        console.log(`${field}: ${obj[field]}`)
+    }
+}
+
+passObject(vasya);
